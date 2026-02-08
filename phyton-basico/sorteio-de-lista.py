d47860491617dc89742519a1ac0fc013fd0fc87f
gerson-bruno <gersonbrunobaptista@gmail.com>
2026-02-08 11:00:21 -0300

    create sorteio-de-lista.py

---
+++ phyton-basico/sorteio-de-lista.py
@@ -0,0 +1,9 @@
+from random import choice
+n1 = input("Digite o nome do aluno: ")
+n2 = input("Digite o nome do aluno: ")
+n3 = input("Digite o nome do aluno: ")
+n4 = input("Digite o nome do aluno: ")
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print(f"O aluno escolhido foi {escolhido}")
+print("Python é top 🔥😎")
