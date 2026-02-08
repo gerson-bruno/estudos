76d094e0831d7d410d25f9aca934ccfa765b2d61
gerson-bruno <gersonbrunobaptista@gmail.com>
2026-02-08 11:08:44 -0300

    create lista-randomica.py

---
+++ phyton-basico/lista-randomica.py
@@ -0,0 +1,9 @@
+from random import shuffle
+n1 = input("Digite o nome: ")
+n2 = input("Digite o nome: ")
+n3 = input("Digite o nome: ")
+n4 = input("Digite o nome: ")
+lista = [n1, n2, n3, n4]
+ordem = shuffle(lista)
+print(f"A ordem de apresentação será: {lista}")
+print("Python é top 🔥😎")
