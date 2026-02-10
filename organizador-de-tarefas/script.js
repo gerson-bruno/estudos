8ef43ee0b2c630aba1482a3adfaf7bb9f0c38fd1
gerson-bruno <gersonbrunobaptista@gmail.com>
2026-02-10 00:15:16 -0300

    Update button

--- organizador-de-tarefas/script.js
+++ organizador-de-tarefas/script.js
@@ -115,9 +115,16 @@ confirmBtn.addEventListener("click", () => {
 
         <div class="card-infos">
             <div class="card-icons">
-                <button class="edit">✏️ Editar</button>
+                <button class="edit">
+                    <svg class="edit-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM21.41 6.34a1.25 1.25 0 0 0 0-1.77l-2.34-2.34a1.25 1.25 0 0 0-1.77 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+                    </svg>
+                    Editar
+                </button>
             </div>
-            <img src="img/avatar.jpg" class="profile">
+            <div class="user">
+                <img src="img/avatar.jpg" class="profile">
+            </div>
         </div>
     `;
 
