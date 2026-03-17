89ba4039eb684dd544b6f8f55c21eae10ae61430
gerson-bruno <gersonbrunobaptista@gmail.com>
2026-03-17 13:38:38 -0300

    calculadoraflex

--- javascript/estudos-autorais/logica-js/to-do-list/index.js
+++ javascript/estudos-autorais/logica-js/to-do-list/index.js
@@ -1,4 +1,4 @@
-document.querySelector(".addbtn").addEventListener("click", () => {
+/* document.querySelector(".addbtn").addEventListener("click", () => {
     const inputValue = document.querySelector(".inputtxt").value;
     const li = document.createElement("li");
     const btn = document.createElement("button");
@@ -10,5 +10,36 @@ document.querySelector(".addbtn").addEventListener("click", () => {
         btn.parentElement.remove()
     })
     document.querySelector(".inputtxt").value = "";
-});
+}); */
 
+/* document.querySelector(".addbtn").addEventListener("click", () => {
+    const inputValue = document.querySelector(".inputtxt").value;
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    li.textContent = inputValue;
+    document.querySelector(".list").appendChild(li);
+    li.appendChild(btn);
+    btn.textContent = "Remover";
+    btn.addEventListener("clik", () => {
+        btn.parentElement.remove()
+    })
+    document.querySelector(".inputtxt").value = "";
+}) */
+
+
+    document.querySelector(".addbtn").addEventListener("click", () => {
+        const inputValue = document.querySelector(".inputtxt").value;
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+
+        li.textContent = inputValue;
+        document.querySelector(".list").appendChild(li);
+
+        li.appendChild(btn);
+        btn.textContent = "Remover";
+        btn.addEventListener("click", () => {
+            btn.parentElement.remove();
+        })
+
+        document.querySelector(".inputtxt").value = "";
+    })
